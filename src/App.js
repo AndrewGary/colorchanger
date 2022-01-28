b8a9f89be98b44b7b26b8bbcdc3b238e156a1f9b
Andrew Gary <andrew.gary91@gmail.com>
2022-01-28 01:09:03 -0600

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,46 @@
-import logo from './logo.svg';
 import './App.css';
+import styled from 'styled-components';
+
+const StyledApp = styled.div`
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+
+  .button{
+    width: 200px;
+    height: 100px;
+
+    border: black 1px solid;
+    display: flex;
+    justify-content: center;
+    align-items: center; 
+  }
+
+`
 
 function App() {
+
+  const handleClick = e => {
+    const r = Math.floor(Math.random() * 256);
+    const g = Math.floor(Math.random() * 256);
+    const b = Math.floor(Math.random() * 256);
+
+    const rgb = `rgb(${r}, ${g}, ${b})`
+    console.log('event: ', e)
+    console.log(rgb);
+    console.log('herheh', e.target.parentElement.style.backgroundColor)
+    e.target.parentElement.style = `background-color: ${rgb}`;
+    
+  }
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <StyledApp>
+        <div className='button' onClick={handleClick}>Click here to change the background color</div>
+      </StyledApp>
     </div>
   );
 }
